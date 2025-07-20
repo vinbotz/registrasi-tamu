0561f851510a211d5a89626f945c1e6d6f4cbaf8
Revin_SI <154691669+vinbotz@users.noreply.github.com>
2025-07-21 04:54:18 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema; // <- Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,9 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Request $request): void
     {
+        // Set default panjang string agar tidak error saat migration
+        Schema::defaultStringLength(191); // <- Tambahkan ini
+
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
         if (env('APP_MAINTENANCE') === 'true') {
             // Bolehkan akses ke halaman login atau admin tertentu (opsional)
             if (!$request->is('admin*')) {
